73fe96fad54d33d3fd29c00be725678c4466aeec
kowalsking <kovvalsking@gmail.com>
2022-10-13 22:54:57 +0300

    Foreground image implement

--- index.js
+++ index.js
@@ -9,22 +9,6 @@ for (let i = 0; i < collisions.length; i += 70) {
   collisionsMap.push(collisions.slice(i, 70 + i))
 }
 
-class Boundary {
-  static width = 48
-  static height = 48
-
-  constructor({ position }) {
-    this.position = position
-    this.width = 48
-    this.height = 48
-  }
-
-  draw() {
-    c.fillStyle = 'rgba(255, 0, 0, 0.0)'
-    c.fillRect(this.position.x, this.position.y, this.width, this.height)
-  }
-}
-
 const boundaries = []
 const offset = { x: -735, y: -650 }
 
@@ -45,34 +29,11 @@ collisionsMap.forEach((row, i) => {
 const image = new Image()
 image.src = './assets/images/Pellet Town.png'
 
+const foregroundImage = new Image()
+foregroundImage.src = './assets/images/foregroundImage.png'
+
 const playerImage = new Image()
 playerImage.src = './assets/images/playerDown.png'
-
-class Sprite {
-  constructor({ position, velocity, image, frames = { max: 1 } }) {
-    this.position = position
-    this.image = image
-    this.frames = frames
-    this.image.onload = () => {
-      this.width = this.image.width / this.frames.max
-      this.height = this.image.height
-    }
-  }
-
-  draw() {
-    c.drawImage(
-      this.image,
-      0,
-      0,
-      this.image.width / this.frames.max,
-      this.image.height,
-      this.position.x,
-      this.position.y,
-      this.image.width / this.frames.max,
-      this.image.height
-    )
-  }
-}
 
 const player = new Sprite({
   position: {
@@ -91,6 +52,14 @@ const background = new Sprite({
   image,
 })
 
+const foreground = new Sprite({
+  position: {
+    x: offset.x,
+    y: offset.y,
+  },
+  image: foregroundImage,
+})
+
 const keys = {
   w: {
     pressed: false,
@@ -106,7 +75,7 @@ const keys = {
   },
 }
 
-const movables = [background, ...boundaries]
+const movables = [background, ...boundaries, foreground]
 
 function rectangularCollision({ rect1, rect2 }) {
   return (
@@ -123,6 +92,7 @@ function animate() {
     boundary.draw()
   })
   player.draw()
+  foreground.draw()
 
   let moving = true
   if (keys.w.pressed && lastKey === 'w') {
