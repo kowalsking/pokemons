304d6ff557b359bda1aa69b5d1fe3a2cbb56da0a
kowalsking <kovvalsking@gmail.com>
2022-11-13 00:24:53 +0200

    enemy fireball rotations

--- classes.js
+++ classes.js
@@ -60,6 +60,9 @@ class Sprite {
     let healthBar = '#enemyHealthBar'
     if (this.isEnemy) healthBar = '#playerHealthBar'
 
+    let rotation = 1
+    if (this.isEnemy) rotation = -2.2
+
     this.health -= attack.damage
 
     switch (attack.name) {
@@ -78,7 +81,7 @@ class Sprite {
             hold: 10
           },
           animate: true,
-          rotation: 1
+          rotation
         })
 
         rendererSprites.splice(1, 0, fireball)
