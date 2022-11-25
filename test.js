a2aeca5c894f94bf0e90cca7c47080a91edeaad3
kowalsking <kovvalsking@gmail.com>
2022-11-25 13:20:00 +0200

    Proxy test commit

--- test.js
+++ test.js
@@ -1,20 +1,31 @@
 const obj = {
-  a: 1
-};
+  a: 1,
+}
 
-const p = new Proxy(obj, {
+const p = new Proxy(Array, {
   get(target, property) {
-    console.log('get target', target);
-    console.log('get property', property);
-    return target[property];
+    console.log('get target', target.name)
+    console.log('get property', target[property])
+    return target[property]
   },
   set(target, property, value) {
-    console.log('set target', target);
-    console.log('set property', property);
-    console.log('set value', value);
-    target[property] = value;
-  }
+    console.log('set target', target)
+    console.log('set property', property)
+    console.log('set value', value)
+    target[property] = value
+  },
 })
 
-p.a = 2
-console.log('a', p.a)
+// p.a = 2
+// console.log('a', p.a)
+
+const test = {
+  length: 0,
+  arr: [],
+  add: function (elem) {
+    p.prototype.push.call(this.arr, elem)
+  },
+}
+
+test.add(1)
+// console.log('test', test) // test { length: 0, arr: [ 1 ], add: [Function: add] }
