955f8fed6951e8464012814b98c9a601f6a33f58
kowalsking <kovvalsking@gmail.com>
2023-01-03 17:44:32 +0200

    init battle

--- battleScene.js
+++ battleScene.js
@@ -8,18 +8,83 @@ const battleBackground = new Sprite({
   },
 })
 
-const draggle = new Monster(monsters.Draggle)
-const emby = new Monster(monsters.Emby)
-
-const rendererSprites = [draggle, emby]
-
-emby.attacks.forEach((attack) => {
-  const button = document.createElement('button')
-  button.innerHTML = attack.name
-  document.querySelector('#attacksBox').append(button)
-})
-
+let draggle
+let emby
+let rendererSprites
 let battleAnimationId
+let queue
+
+function initBattle() {
+  draggle = new Monster(monsters.Draggle)
+  emby = new Monster(monsters.Emby)
+
+  rendererSprites = [draggle, emby]
+  queue = []
+
+  emby.attacks.forEach((attack) => {
+    const button = document.createElement('button')
+    button.innerHTML = attack.name
+    document.querySelector('#attacksBox').append(button)
+  })
+
+  // event listeners for our buttons (attack and fireball)
+  const buttons = document.querySelectorAll('button')
+  buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      const selectedAttack = attacks[e.currentTarget.innerHTML]
+      emby.attack({
+        attack: selectedAttack,
+        recipient: draggle,
+        rendererSprites,
+      })
+
+      if (draggle.health <= 0) {
+        queue.push(() => {
+          draggle.faint()
+        })
+        queue.push(() => {
+          // fade back to black
+          gsap.to('#overlappingDiv', {
+            opacity: 1,
+            onComplete: () => {
+              window.cancelAnimationFrame(battleAnimationId)
+              animate()
+
+              document.querySelector('#userInterface').style.display = 'none'
+              gsap.to('#overlappingDiv', {
+                opacity: 0,
+              })
+            },
+          })
+        })
+      }
+
+      // draggle or enemy attacks right here
+      const randomAttack =
+        draggle.attacks[Math.floor(Math.random() * draggle.attacks.length)]
+
+      queue.push(() => {
+        draggle.attack({
+          attack: randomAttack,
+          recipient: emby,
+          rendererSprites,
+        })
+
+        if (emby.health <= 0) {
+          queue.push(() => {
+            emby.faint()
+          })
+        }
+      })
+    })
+
+    button.addEventListener('mouseenter', (e) => {
+      const selectedAttack = attacks[e.currentTarget.innerHTML]
+      document.querySelector('#attackType').innerHTML = selectedAttack.type
+      document.querySelector('#attackType').style.color = selectedAttack.color
+    })
+  })
+}
 
 function animateBattle() {
   battleAnimationId = window.requestAnimationFrame(animateBattle)
@@ -30,67 +95,8 @@ function animateBattle() {
   })
 }
 
+initBattle()
 animateBattle()
-
-const queue = []
-
-// event listeners for our buttons (attack and fireball)
-const buttons = document.querySelectorAll('button')
-buttons.forEach((button) => {
-  button.addEventListener('click', (e) => {
-    const selectedAttack = attacks[e.currentTarget.innerHTML]
-    emby.attack({
-      attack: selectedAttack,
-      recipient: draggle,
-      rendererSprites,
-    })
-
-    if (draggle.health <= 0) {
-      queue.push(() => {
-        draggle.faint()
-      })
-      queue.push(() => {
-        // fade back to black
-        gsap.to('#overlappingDiv', {
-          opacity: 1,
-          onComplete: () => {
-            window.cancelAnimationFrame(battleAnimationId)
-            animate()
-
-            document.querySelector('#userInterface').style.display = 'none'
-            gsap.to('#overlappingDiv', {
-              opacity: 0
-            })
-          }
-        })
-      })
-    }
-
-    // draggle or enemy attacks right here
-    const randomAttack =
-      draggle.attacks[Math.floor(Math.random() * draggle.attacks.length)]
-
-    queue.push(() => {
-      draggle.attack({
-        attack: randomAttack,
-        recipient: emby,
-        rendererSprites,
-      })
-
-      if (emby.health <= 0) {
-        queue.push(() => {
-          emby.faint()
-        })
-      }
-    })
-  })
-
-  button.addEventListener('mouseenter', (e) => {
-    const selectedAttack = attacks[e.currentTarget.innerHTML]
-    document.querySelector('#attackType').innerHTML = selectedAttack.type
-    document.querySelector('#attackType').style.color = selectedAttack.color
-  })
-})
 
 document.querySelector('#dialogueBox').addEventListener('click', (e) => {
   if (queue.length > 0) {
